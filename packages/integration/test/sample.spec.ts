aa9544ce4444154a9c447a9616f2d0ec064a6e40
Dhinesh <dhinesh.ravi@gmail.com>
2023-01-10 14:13:04 +0530

    Add more tests

--- packages/integration/test/sample.spec.ts
+++ packages/integration/test/sample.spec.ts
@@ -7,29 +7,60 @@ const harbor = new Harbor({
   projectKey: "xkfSjdSLuFnoAuVccX7j22"
 });
 
-export default function getTestnetName(){
+export default function getTestnetName() {
   return new Promise((resolve, reject) => {
     getLastCommit((err, commit) => {
       if (err) {
         reject(err);
       } else {
-        const testnetName = "sha-" + commit.hash.slice(0, 7); 
+        const testnetName = "sha-" + commit.hash.slice(0, 7);
         resolve(testnetName);
       }
     });
   });
 }
 
-describe('sample', function() {
-  it('add', async function() {
-    let result = 7;
-    expect(result).to.equal(7);
+describe('Harbor Test E2E', function () {
+
+  it('Testnet Info', async function () {
     let testnetName = await getTestnetName();
     await harbor.authenticate();
     console.log("\n\n==========testnet==========");
     if (typeof testnetName === 'string') {
       const testnet = await harbor.testnet(testnetName);
       console.log(testnet);
+
+      expect(testnet.status).to.equal("RUNNING");
+
+      const chains = await testnet.chains();
+      console.log(`\n\n==========chains(${chains.length})==========`);
+
+      chains.forEach((chain) => {
+        // console.log(chain)
+        expect(chain.status).to.equal("RUNNING");
+        console.log(`${chain.chain} - ${chain.id} - ${chain.status} - ${chain.endpoint}`);
+        chain.accounts().then((accounts) => {
+          console.log(`\n\n==========accounts==========`);
+          accounts.forEach((account, idx) => {
+            // console.log(account)
+            console.log(`=========${chain.chain}-account-#${idx + 1}/${accounts.length}==========`);
+            console.log(`${account.address}  - ${account.type} - ${JSON.stringify(account.balances)} - ${JSON.stringify(account.abi)}`);
+          });
+        }).catch((err) => {
+          console.error(err);
+        });
+      })
+
+      const offChainActors = await testnet.offChainActors();
+      console.log(`\n\n==========offChainActors(${offChainActors.length})==========`);
+      console.log(offChainActors)
+      offChainActors.forEach(async (actor) => {
+        expect(actor.status).to.equal("RUNNING");
+        console.log(`${actor.name} - ${actor.status} - ${actor.ports()} - ${actor.endpoint}`);
+        const logs = await actor.logs();
+        console.log(`\n\n==========logs for actor ${actor.name}==========`);
+        console.log(logs);
+      });
     }
   });
 });
